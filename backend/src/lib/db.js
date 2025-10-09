13279fb7f2c90e764c86437d382b5bc6048dfc46
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-10-10 04:26:24 +0700

    config: run CI/CD pipeline ver12

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () => {
       console.log("MongoDB connected");
     });
-    await mongoose.connect(`${process.env.DATABASE_URL}`);
+    await mongoose.connect(`${process.env.DATABASE_URI}`);
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
   }
